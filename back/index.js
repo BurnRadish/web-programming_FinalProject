5125ba4156b82879b05a5737b5af216d011daa93
Piya Kaewket <piya.pkk@outlook.com>
2021-04-19 20:22:28 +0700

    Update Config & install extension

---
+++ back/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+var cors = require('cors')
+const path = require("path")
+const app = express();
+app.use(cors())
+
+// Statics
+app.use(express.static('static'))
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// routers
+const empRouter = require('./routes/employees')
+const partnerRouter = require('./routes/partner')
+app.use(empRouter.router)
+app.use(partnerRouter.router)
+
+app.listen(3000, () => {
+    console.log(`Example app listening at http://localhost:3000`)
+})

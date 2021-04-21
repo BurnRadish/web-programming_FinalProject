30765ca4763a12e4c06d6e6bf5f03c5d89671914
Piya Kaewket <piya.pkk@outlook.com>
2021-04-21 21:09:55 +0700

    refactoring code

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -1,52 +1,46 @@
 <template>
     <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="/home">
-      <img src="../assets/WAP_LOGO.png" width="53" height="52" class='pr-3'>
-      WAP MACHINERY
-    </a>
+        <div class="navbar-brand ">
+            <a class="navbar-item" href="/home">
+                <img src="../assets/WAP_LOGO.png" width="53" height="52" class='pr-3'>
+                WAP MACHINERY
+            </a>
     
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-
-      <a class="navbar-item" href='/employees'>
-        Employees
-      </a>
-
-      <a class="navbar-item" href='/partners'>
-        Partner
-      </a>
-
-      <a class="navbar-item" href='/inventory'>
-        Inventory
-      </a>
-      </div>
-    <div class='navbar-end'>
-      <div class="navbar-item navbar-end has-dropdown is-hoverable" >
-        <a class="navbar-link">
-          Setting
-        </a>
-
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            Profile
-          </a>
-          <a class="navbar-item">
-            Logout
-          </a>
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
         </div>
-      </div>
-    </div>
-    
-  </div>
-</nav>
+        <div id="navbarBasicExample" class="navbar-menu ">
+            <div class="navbar-start">
+                <a class="navbar-item" href='/employees'>
+                    Employees
+                </a>
+                <a class="navbar-item" href='/partners'>
+                    Partner
+                </a>
+                <a class="navbar-item" href='/inventory'>
+                    Inventory
+                </a>
+            </div>
+            <div class='navbar-end'>
+                <div class="navbar-item navbar-end has-dropdown is-hoverable" >
+                    <a class="navbar-link">
+                        Setting
+                    </a>
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item">
+                            Profile
+                        </a>
+                        <a class="navbar-item">
+                            Logout
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>

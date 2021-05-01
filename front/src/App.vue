618d325f99dcb45e7de869af2f165ebf2b630ccd
Piya Kaewket <41195318+IJerickoZ@users.noreply.github.com>
2021-05-01 20:31:34 +0700

    Update

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,35 +1,5 @@
 <template>
   <div id="app">
-    <!--<router-view :key="$route.fullPath" />-->
-    <router-view :key="$route.fullPath" @auth-change="onAuthChange" />
+    <router-view :key="$route.fullPath" />
   </div>
 </template>
-
-<script>
-import axios from "axios";
-
-export default {
-  data() {
-    return {
-      user: null,
-    };
-  },
-  mounted() {
-    this.onAuthChange();
-  },
-  methods: {
-    onAuthChange() {
-      const token = localStorage.getItem("token");
-      if (token) {
-        this.getUser();
-      }
-    },
-    getUser() {
-      //const token = localStorage.getItem("token");
-      axios.get("/user/me").then((res) => {
-        this.user = res.data;
-      });
-    },
-  },
-};
-</script>

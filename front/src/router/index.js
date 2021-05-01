13cf967ff938411a85fd007ddcb30dbebc8018d9
Piya Kaewket <41195318+IJerickoZ@users.noreply.github.com>
2021-05-01 20:22:43 +0700

    Navigation Guard

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -7,26 +7,31 @@ const routes = [
   {
     path: '/',
     name: 'Login',
+    meta: { guess: true },
     component: () => import('../views/Login.vue')
   },
   {
     path: '/inventory',
     name: 'Inventory',
+    meta: { login: true },
     component: () => import('../views/Inventory.vue')
   },
   {
     path: '/Employees',
     name: 'Employees',
+    meta: { login: true },
     component: () => import('../views/Employee.vue')
   },
   {
     path: '/partners',
     name: 'Partners',
+    meta: { login: true },
     component: () => import('../views/Partner.vue')
   },
   {
     path: '/home',
     name: 'Transaction',
+    meta: { login: true },
     component: () => import('../views/Transaction.vue')
   },
 ]
@@ -35,6 +40,22 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = !!localStorage.getItem('token')
+
+  if (to.meta.login && !isLoggedIn){
+    alert('Please login first!')
+    next({ path: '/' })
+  }
+
+  if (to.meta.guess && isLoggedIn){
+    alert("You've already logged in")
+    next({ path: '/home'})
+  }
+
+  next()
 })
 
 export default router

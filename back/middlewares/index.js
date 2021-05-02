8e5db42bb8905d6e022e1b5ae081493ede9dec44
Piya Kaewket <41195318+IJerickoZ@users.noreply.github.com>
2021-05-02 19:47:58 +0700

    Update isAdmin

--- back/middlewares/index.js
+++ back/middlewares/index.js
@@ -26,6 +26,18 @@ async function isLoggedIn(req, res, next) {
   next()
 }
 
+const isAdmin = async (req, res, next) => {
+  if (req.user.role === 'admin'){
+    return next()
+  }
+  else {
+    return res.status(403).send('You do not have permission to perform this action')
+  }
+
+  next()
+}
+
 module.exports = {
   isLoggedIn,
+  isAdmin
 };

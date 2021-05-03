4efb914b3f0cec234f66458df0bb7222b231bfd5
Piya Kaewket <piya.pkk@outlook.com>
2021-05-04 04:21:09 +0700

    Update App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,11 +5,4 @@
 </template>
 
 <style>
-body {
-  background-image: linear-gradient(rgb(255, 255, 255), rgb(119, 167, 255));
-  height: 100%;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
 </style>
